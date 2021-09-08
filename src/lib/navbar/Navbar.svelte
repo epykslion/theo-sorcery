aa364feb17f941755cfab9fa15eb4573aa34516c
Theodore Nikolai Idris <theonikolaii@gmail.com>
2021-09-08 19:17:33 +0800

    Optimized links and icons

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -1,30 +1,30 @@
 
 <nav class="fixed w-screen z-50 bg-coolGray-50 shadow-md">
   <div class="flex justify-between max-w-screen-lg items-center mx-auto p-2">
-    <a href="/" class='font-extralight text-center tracking-widest leading-4 text-[0.9rem] uppercase inline-block'>Theo <br/><span class="text-[0.975rem]">Niko</span></a>
+    <a sveltekit:prefetch href="/" class='font-extralight text-center tracking-widest leading-4 text-[0.9rem] uppercase inline-block'>Theo <br/><span class="text-[0.975rem]">Niko</span></a>
 <div class="flex justify-evenly text-[0.85rem] items-center">
-      <a href="/" class="mx-[0.2rem] sm:mx-8">
+      <a sveltekit:prefetch href="/" class="mx-[0.2rem] sm:mx-8">
               Home
             </a> 
-      <a href="/work" class="mx-[0.2rem] sm:mx-8">
+      <a sveltekit:prefetch href="/work" class="mx-[0.2rem] sm:mx-8">
               Work
             </a> 
-      <a href="/about" class="mx-[0.2rem] sm:mx-8">
+      <a sveltekit:prefetch href="/about" class="mx-[0.2rem] sm:mx-8">
               About
             </a> 
-      <a href="/contact" class="mx-[0.2rem] sm:mx-8">
+      <a sveltekit:prefetch href="/contact" class="mx-[0.2rem] sm:mx-8">
               Contact
             </a>
   </div> 
   <div class="inline-flex w-1/5 space-x-2 items-center sm:space-x-6 sm:scale-150 justify-end">
-  <a href="https://twitter.com/theonikomao">
-      <i class="fab fa-twitter text-gray-600 hover:text-gray-400 inline-block"></i>
+  <a rel='external' href="https://twitter.com/theonikomao">
+      <i rel='preload' class="fab fa-twitter text-gray-600 hover:text-gray-400 inline-block"></i>
     </a>
-      <a href="https://linkedin.com/in/theonikolai">
-    <i class="fab fa-linkedin text-gray-600 hover:text-gray-400 inline-block"></i>
+      <a rel='external' href="https://linkedin.com/in/theonikolai">
+    <i rel='preload' class="fab fa-linkedin text-gray-600 hover:text-gray-400 inline-block"></i>
     </a>
-    <a href="https://github.com/theonikolai/theo-sorcery">
-      <i class="fab fa-github text-gray-600 hover:text-gray-400 inline-block"></i>
+    <a rel='external' href="https://github.com/theonikolai/theo-sorcery">
+      <i rel='preload' class="fab fa-github text-gray-600 hover:text-gray-400 inline-block"></i>
     </a>
   </div>
   </div>
