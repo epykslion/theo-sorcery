64157f941dade7d099b8855ae42c4cbe44d8f907
Theodore Nikolai Idris <theonikolaii@gmail.com>
2021-09-08 18:06:58 +0800

    Transmute to Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
-//import adapter from '@sveltejs/adapter-netlify';
-import vercel from '@sveltejs/adapter-vercel';
+import netlify from '@sveltejs/adapter-netlify';
+// import vercel from '@sveltejs/adapter-vercel';
 const config = {
   kit: {
-    adapter: vercel(),
+    adapter: netlify(),
     target: '#svelte'
   },
   extensions: ['.svelte', ...mdsvexConfig.extensions],
